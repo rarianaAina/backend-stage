d7836a48a6bb456bf72450524e5d32bb4955752d
Web dev <141223950+rarianaAina@users.noreply.github.com>
2025-10-08 10:11:52 +0300

    Added CORRECTIONS_SCHEMA.md

--- src/main/java/com/nrstudio/portail/domaine/Produit.java
+++ src/main/java/com/nrstudio/portail/domaine/Produit.java
@@ -1,120 +1,38 @@
 package com.nrstudio.portail.domaine;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "produit", schema = "dbo")
+@Getter
+@Setter
 public class Produit {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
-  @Column(name = "nom", length = 255, nullable = false)
-  private String nom;
+  @Column(name="id_externe_crm", length=100)
+  private String idExterneCrm;
 
-  @Column(name = "description", columnDefinition = "TEXT")
+  @Column(name="code_produit", length=100)
+  private String codeProduit;
+
+  @Column(nullable=false, length=250)
+  private String libelle;
+
+  @Lob
   private String description;
 
-  @Column(name = "reference", length = 100)
-  private String reference;
+  @Column(nullable=false)
+  private boolean actif = true;
 
-  @Column(name = "categorie", length = 100)
-  private String categorie;
-
-  @Column(name = "version", length = 50)
-  private String version;
-
-  @Column(name = "actif")
-  private Boolean actif;
-
-  @Column(name = "date_creation")
+  @Column(name="date_creation", nullable=false)
   private LocalDateTime dateCreation;
 
-  @Column(name = "date_mise_a_jour")
+  @Column(name="date_mise_a_jour", nullable=false)
   private LocalDateTime dateMiseAJour;
-
-  @Column(name = "id_externe_crm", unique = true)
-  private Integer idExterneCrm;
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getNom() {
-    return nom;
-  }
-
-  public void setNom(String nom) {
-    this.nom = nom;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public String getReference() {
-    return reference;
-  }
-
-  public void setReference(String reference) {
-    this.reference = reference;
-  }
-
-  public String getCategorie() {
-    return categorie;
-  }
-
-  public void setCategorie(String categorie) {
-    this.categorie = categorie;
-  }
-
-  public String getVersion() {
-    return version;
-  }
-
-  public void setVersion(String version) {
-    this.version = version;
-  }
-
-  public Boolean getActif() {
-    return actif;
-  }
-
-  public void setActif(Boolean actif) {
-    this.actif = actif;
-  }
-
-  public LocalDateTime getDateCreation() {
-    return dateCreation;
-  }
-
-  public void setDateCreation(LocalDateTime dateCreation) {
-    this.dateCreation = dateCreation;
-  }
-
-  public LocalDateTime getDateMiseAJour() {
-    return dateMiseAJour;
-  }
-
-  public void setDateMiseAJour(LocalDateTime dateMiseAJour) {
-    this.dateMiseAJour = dateMiseAJour;
-  }
-
-  public Integer getIdExterneCrm() {
-    return idExterneCrm;
-  }
-
-  public void setIdExterneCrm(Integer idExterneCrm) {
-    this.idExterneCrm = idExterneCrm;
-  }
 }
