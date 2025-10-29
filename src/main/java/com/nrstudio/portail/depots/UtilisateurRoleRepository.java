dd78679828d1d62232ee1726c748539106e6f29e
Rariana ARR. ANDRIAMIADANA <arr@optimada.mg>
2025-10-29 13:02:14 +0100

    update userRole repo

--- src/main/java/com/nrstudio/portail/depots/UtilisateurRoleRepository.java
+++ src/main/java/com/nrstudio/portail/depots/UtilisateurRoleRepository.java
@@ -3,7 +3,23 @@ package com.nrstudio.portail.depots;
 
 import com.nrstudio.portail.domaine.UtilisateurRole;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UtilisateurRoleRepository extends JpaRepository<UtilisateurRole, Integer> {
+    @Query("SELECT ur FROM UtilisateurRole ur WHERE ur.utilisateur.id = :utilisateurId")
+    List<UtilisateurRole> findByUtilisateurId(@Param("utilisateurId") Integer utilisateurId);
+    
+    @Query("SELECT ur FROM UtilisateurRole ur WHERE ur.role.id IN :roleIds")
+    List<UtilisateurRole> findByRoleIdIn(@Param("roleIds") List<Integer> roleIds);
+    
+    @Query("SELECT CASE WHEN COUNT(ur) > 0 THEN true ELSE false END " +
+           "FROM UtilisateurRole ur " +
+           "WHERE ur.utilisateur.id = :utilisateurId " +
+           "AND ur.role.id IN :roleIds")
+    boolean existsByUtilisateurIdAndRoleIdIn(@Param("utilisateurId") Integer utilisateurId, 
+                                           @Param("roleIds") List<Integer> roleIds);
+
 }
